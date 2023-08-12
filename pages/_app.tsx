40b3cec35f0e1716678cbbf4aba5cfc6bdbd30bd
Muaadh Mohamed <muaadhhd@gmail.com>
2023-08-12 03:47:24 -0400

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,67 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+  lightTheme,
+} from "@rainbow-me/rainbowkit";
+import type { AppProps } from "next/app";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import {
+  arbitrum,
+  goerli,
+  mainnet,
+  optimism,
+  polygon,
+  zora,
+  sepolia,
+} from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    zora,
+    goerli,
+    sepolia,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [goerli] : []),
+  ],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "buymeacrypto",
+  projectId: "18c4fce12d591123893a43169198ebbe",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        theme={lightTheme({
+          accentColor: "#828282",
+          accentColorForeground: "white",
+          fontStack: "system",
+          overlayBlur: "small",
+        })}
+        modalSize="compact"
+        chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
